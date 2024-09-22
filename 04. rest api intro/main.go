1488443d6bfeedc7c8b6704693e68008a338874c
ayoadeoye1 <isadeoye02@gmail.com>
2024-09-22 23:22:25 +0100

    grading with score

--- 04. rest api intro/main.go
+++ 04. rest api intro/main.go
@@ -7,27 +7,50 @@ import (
 	"net/http"
 )
 
-const minScore = 45
+const minScore = 30
 const maxScore = 100
 
 type student_score struct {
   FinalScore int `json:"student_score"`
+  Grade string `json:"grade"`
+}
+
+func getGrade (score uint) (string) {
+	x := ""
+
+	if score >= 70 && score <= 100 {
+		x = "A"
+	} else if score >= 60 && score < 70 {
+		x = "B"
+	} else if score >= 50 && score < 60 {
+		x = "C"
+	} else if score >= 45 && score < 50 {
+		x = "D"
+	} else if score >= 40 && score < 45 {
+		x = "E"
+	} else if score < 40 {
+		x = "F"
+	}
+
+	return x
 }
 
 func getScore(w http.ResponseWriter, r *http.Request) {
-  var finalScore = student_score{
-      FinalScore: (rand.Intn(maxScore-minScore) + minScore),
-  }
+	var score = rand.Intn(maxScore-minScore) + minScore
+	var finalScore = student_score{
+		FinalScore: score,
+		Grade: getGrade(uint(score)),
+	}
 
-  w.WriteHeader(http.StatusOK)
-  json.NewEncoder(w).Encode(finalScore)
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(finalScore)
 }
 
 func handleRequests() {
-  http.Handle("/creditscore", http.HandlerFunc(getScore))
-  log.Fatal(http.ListenAndServe(":8080", nil))
+	http.Handle("/score", http.HandlerFunc(getScore))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func main() {
-  handleRequests()
+  	handleRequests()
 }
