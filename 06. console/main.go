d466c6ff74c1c36d2eab075b13bc883edde21619
ayoadeoye1 <ayoadeoye.isaac@gmail.com>
2024-11-03 20:13:52 +0100

    feat: passing log matrix function via go concurency

--- 06. console/main.go
+++ 06. console/main.go
@@ -3,16 +3,25 @@ package main
 import "fmt"
 
 func logMatrix(x int) {
-	mainArr := make([][]int, 0)
-	for i := 0; i < x; i++ {
-		subArr := make([]int, 0)
-		for j := 1; j < x; j++ {
-			subArr = append(subArr, i*j)
-		}
-		mainArr = append(mainArr, subArr)
-	}
 
-	fmt.Print(mainArr)
+	ch := make(chan [][]int)
+
+	go func() {
+		mainArr := make([][]int, 0)
+		for i := 0; i < x; i++ {
+			subArr := make([]int, 0)
+			for j := 1; j < x; j++ {
+				subArr = append(subArr, i*j)
+			}
+			mainArr = append(mainArr, subArr)
+		}
+
+		ch <- mainArr
+	}()
+
+	chanVal := <-ch
+
+	fmt.Print(chanVal)
 }
 
 func main() {
