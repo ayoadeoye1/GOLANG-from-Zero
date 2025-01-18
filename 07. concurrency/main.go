67f89416a3d5917775d588b713c925d1902fb7ea
ayoadeoye1 <ayoadeoye.isaac@gmail.com>
2025-01-18 23:14:17 +0100

    feat: check website

--- 07. concurrency/main.go
+++ 07. concurrency/main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func checkAPI(api string, ch chan string) {
+	_, err := http.Get(api)
+	if err != nil {
+		ch <- fmt.Sprintf("ERROR: %s is down!\n", api)
+		return
+	}
+	ch <- fmt.Sprintf("SUCCESS: %s is up and running!\n", api)
+}
